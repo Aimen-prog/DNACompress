1da6970fc289725303e4fb17b53dd63c0c995a7c
Aimen CHERIF <cherif_aymenn@hotmail.com>
2022-02-13 00:41:25 +0100

    unsorted and sorted lists determined

--- bwt.py
+++ bwt.py
@@ -30,43 +30,28 @@ class BurrowsWheeler :
         previous_sequence = self.sequence
         
         # Initialization of the BWT construction matrix
-        contruction_matrix=[self.sequence]
+        construction_matrix=[self.sequence]
         
         # Completing the BWT construction matrix      
         for i in range (0, seq_len-1, 1) :
             next_sequence=previous_sequence[-1] + previous_sequence[0:seq_len-1]
             
             #adding it to the matrix      
-            contruction_matrix.append(next_sequence)
+            construction_matrix.append(next_sequence)
 
             #previous variable takes this sequence for next iteration
             previous_sequence = next_sequence
         
         # Sorting the BWT construction matrix
-        construction_matrix= sorted(contruction_matrix)
+        construction_matrix_sorted= sorted(construction_matrix)
         
         # Getting the BWT sequence as the accumulation of the last character of each row
         bwt = ""
-        for row in construction_matrix :
+        for row in construction_matrix_sorted :
             bwt+=row[-1]
 
-        #return bwt
-        print(bwt)
-        print(construction_matrix)
 
-
-
-if __name__=='__main__':
-    b = BurrowsWheeler('AGTAC')
-    b.bwt_construction()
-    
-
-
-
-
-
-
-
+        return bwt
 
 
 
