3e1053d8236cf945ad9ee6f36c749d49b5be89b3
Aimen CHERIF <cherif_aymenn@hotmail.com>
2022-02-13 19:09:07 +0100

    constructor setting

--- huffman_tree_node.py
+++ huffman_tree_node.py
@@ -11,6 +11,33 @@ class TreeNode :
 
     """Class for tree node"""
  
-    def __init__(self) :
+    def __init__(self, char: str, value: int):
+        """
+        Class constructor
+        """
+        self.char = char
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def is_leaf(self):
+        
+        """
+            method to check if a node is a leaf or not
+            a leaf is a single node with nothing on his right and left
+        """
         pass
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
